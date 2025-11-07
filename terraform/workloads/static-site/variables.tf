e1e241b39319c2322bad72a7d16fef83215870a4
Eoin Fitzpatrick <efitzpatrick@vibrant.org>
2025-11-07 10:46:31 -0500

    revert bucket versioning for compat

--- terraform/workloads/static-site/variables.tf
+++ terraform/workloads/static-site/variables.tf
@@ -62,7 +62,7 @@ variable "force_destroy_bucket" {
 variable "enable_versioning" {
   description = "Enable S3 bucket versioning"
   type        = bool
-  default     = false
+  default     = true
 }
 
 variable "enable_cross_region_replication" {
